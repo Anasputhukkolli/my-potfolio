3a3f711bee6657f5313496a4ad2d0b262b6a10cd
Anasputhukkolli <anasputhukkolli123456@gmail.com>
2026-06-02 21:06:37 +0530

    name commit

--- anas/components/Sidebar/Sidebar.tsx
+++ anas/components/Sidebar/Sidebar.tsx
@@ -178,7 +178,7 @@ export default function Sidebar() {
                     margin: 0,
                   }}
                 >
-                  Anas Puthukkolli
+                  Muhammed Anas 
                 </h1>
                 <p
                   style={{
